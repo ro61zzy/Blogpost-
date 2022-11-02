f31d968736d51be548d463b8617ad4d3911f63d1
Rose Karemeri <rosekaremeri@gmail.com>
2022-11-02 19:05:06 +0300

    express, mongodb n nodejs setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+//set up express
+//middleware
+app.use(express.json())
+
+app.listen(3001, () => {
+  console.log('Server is running on port 3001')
+})
+
+//set up mongodb with mongoose
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/CRUD',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Connected to MongoDB')
+    }
+  }
+)
+
+module.exports = app
